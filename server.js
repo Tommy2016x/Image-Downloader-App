560343d898753d1708e12d128067f4b3d1052606
Tommy <carrascalx@gmail.com>
2018-09-23 17:59:06 -0400

    added some new features

--- server.js
+++ server.js
@@ -3,21 +3,54 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const imagesearch = require('image-search-results')
 const downloader = require('image-downloader');
+const tmp = require('tmp');
+const archiver = require('archiver');
+const fs = require('fs');
+const cors = require('cors');
+
 
 const app = express();
 
 app.use(bodyparser.json());
+app.use(cors())
 app.listen(3000);
 
 app.post('/shot', async (req,res) => {
+    let archive = archiver('zip')
+    let output = fs.createWriteStream(__dirname + 'target.zip');
+    output.on('close', function() {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+      });
+      output.on('end', function() {
+        console.log('Data has been drained');
+      });
+       
+      archive.on('warning', function(err) {
+        if (err.code === 'ENOENT') {
+          // log warning
+        } else {
+          // throw error
+          throw err;
+        }
+      });
+
+      archive.on('error', function(err) {
+        throw err;
+      });
+      
+
+    console.log(req.body)
     const { images } = req.body;
     let urls = [];
+    let dir = tmp.dirSync();
 
     for(let img of images){
+        
         let url = await imagesearch.google({query:img});
         let options = {
             url: url[0].url,
-            dest: './images'
+            dest: dir.name
         }
         console.log(img)
 
@@ -26,5 +59,10 @@ app.post('/shot', async (req,res) => {
         urls.push(url[0].url)
     }
 
-    res.send(urls)
+    archive.pipe(output);
+    archive.directory(dir.name,false)
+    archive.finalize();
+    res.download(__dirname + '/downloads/txt.txt','txt.txt');
+    res.send('sent')
+    console.log(dir.name);
 })
